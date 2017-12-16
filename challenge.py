bfbfb3e0d413bfe7945017e1253eae064fd117f1
r <radu@raicea.com>
2017-12-16 10:28:54 -0500

    simplify from depth-first traversal to simple recursion

--- challenge.py
+++ challenge.py
@@ -14,73 +14,64 @@ Dependencies:
 External libraries can be installed with: [sudo] pip3 install <library1_name> [<library2_name> ...]
 
 This script is called with the following format:
-python challenge.py [<url>]
+python challenge.py [<url_index>]
 
 Example:
 $ python challenge.py
 or
-$ python challenge.py https://backend-challenge-summer-2018.herokuapp.com/challenges.json?id=2
+$ python challenge.py 2
 ~~~~~~~~~~~~
 
 Solution logic:
 1. Request the API data and process it into one dictionary object.
 2. Loop over all the menus to find which ones are parents (lack of "parent_id" key).
-3. Using a depth-first traversal algorithm on the parent nodes, create new menu dictionary objects
-   matching the desired output format (containing the "root_id" and "children" keys).
-4. Insert each new menu dictionary into "valid_menus" if the "root_id" value is not in the "children"
-   list (no cyclical reference), and if the maximum depth is not greater than 4. Otherwise, insert
-   it into "invalid_menus".
-
-Note:
-A single node (root node) is considered to have a depth of 0. A depth of 4 requires the path to have 5 nodes and 4 edges.
-Depth 0: O
-Depth 1: O--O
-Depth 4: O--O--O--O--O
+3. Using recursion, get the depth of the longest branch starting at a parent node, and get all its children.
+4. Classify menus between "valid_menus" (parent is not in children and max depth is <= 4) and "invalid_menus".
 """
 
-import json
 import sys
 
 import requests
 
-DEFAULT_URL = 'https://backend-challenge-summer-2018.herokuapp.com/challenges.json?id=1'
+URL = {
+    '1': 'https://backend-challenge-summer-2018.herokuapp.com/challenges.json?id=1',
+    '2': 'https://backend-challenge-summer-2018.herokuapp.com/challenges.json?id=2'
+}
 
 
-def get_api_data(url):
+def get_api_data(url_index):
     """
     Fetches the API data from all the pages and processes it into a dictionary.
 
-    :param str url: URL like https://domain.com/challenges.json?id=1
-    :return dict: Dictionary (with keys being the id of the menu) of all the menu dictionaries
+    :param str url_index: URL index
+    :return list: List of all the menu dictionaries
     """
 
     page = 1
     menus = []
 
     while True:
-        r = requests.get(url + f'&page={page}').json()
+        r = requests.get(URL[url_index] + f'&page={page}').json()
         if r['menus'] == []:
             break
         else:
             menus += r['menus']
             page += 1
 
-    menus_dict = {menu['id']: menu for menu in menus} # The key to each menu is its id
-
-    return menus_dict
+    return menus
 
 
 def find_parents(menus):
     """
     Finds parent menus (menus with no "parent_id" key).
 
-    :param dict menus: Dictionary of menu dictionaries
-    :return list: List of dictionaries of menus that are parents
+    :param list menus: List of menu dictionaries
+    :return list: List of menu dictionaries that are parents
     """
 
     parents = []
 
-    for menu in menus.values():
+    for menu in menus:
         parent = menu.get('parent_id')
         if parent is None:
             parents.append(menu)
@@ -92,29 +83,22 @@ def get_children(menus, parents):
     """
     Uses a depth-first traversal algorithm to find all the children of parent menus.
 
-    :param dict menus: Dictionary of menu dictionaries
+    :param list menus: List of menu dictionaries
     :param list parents: List of parent menu dictionaries
     :return list: List of branches (parent menus with their children)
     """
 
+    menus_dict = {menu['id']: menu for menu in menus} # Indexes the menus list to a dictionary with the IDs as keys to easily traverse it
+
     branches = []
 
     for parent in parents:
-        visited = set()
-        stack = []
-        stack.extend(parent['child_ids'])
-
-        while stack:
-            node = stack.pop()
-            if node not in visited:
-                visited.add(node)
-                not_visited_children = set(menus[node]['child_ids']) - visited
-                stack.extend(not_visited_children)
+        max_depth, children = _get_max_depth(menus_dict, parent['id'], parent['child_ids'])
 
         branches.append({
             'root_id': parent['id'],
-            'children': list(visited),
-            'max_depth': _get_max_depth(menus, parent['id'], parent['child_ids'])
+            'children': list(children),
+            'max_depth': max_depth
         })
 
     return branches
@@ -122,31 +106,36 @@ def get_children(menus, parents):
 
 def _get_max_depth(menus, parent, children):
     """
-    Gets the maximum depth of a node recursively.
+    Gets the maximum depth and list of children of a parent node recursively.
 
     :param dict menus: Dictionary of menu dictionaries
-    :param int parent: ID of the parent menu
-    :param list children: List of the IDs of all the children of the next node to compute the depth of
+    :param int parent: ID of the parent menu (root of branch)
+    :param list children: List of the IDs of all the children of the next node
     :return int: Maximum depth of a parent menu
+    :return list: List of all children of the parent node
     """
     
-    if children == []:
-        return 0
+    all_children = set()
+    all_children.update(children)
+
+    if children == []: # The case where we've reached a leaf
+        return 0, all_children
     elif parent in children: # The case of a cyclical reference
-        return 1
+        return 1, all_children
     else:
         depths = {}
         for child in children:
-            depths[child] = _get_max_depth(menus, parent, menus[child]['child_ids']) # Gets the depth of each child
+            depths[child], new_children = _get_max_depth(menus, parent, menus[child]['child_ids']) # Gets the depth of each child
+            all_children.update(new_children)
 
-        return max(depths.values()) + 1
+        return max(depths.values()) + 1, all_children
 
 
-def validate_parents(branches):
+def validate_menus(branches):
     """
-    Validates parent menus as valid if there is no cyclical reference and if the depth is at most 4.
+    Validates parent menus as valid if there is no cyclical reference and if the maximum depth is at most 4.
 
-    :param list branches: List of menu branches (parent menus with their children)
+    :param list branches: List of menu branches (parent menus with all their children)
     :return dict: Dictionary with a list of valid menus and a list with invalid menus
     """
     
@@ -166,22 +155,20 @@ def validate_parents(branches):
 
 def main():
     try:
-        url = sys.argv[1] # Gets the url from the command line call
+        url_index = sys.argv[1] # Gets the url from the command line call
     except IndexError:
-        url = DEFAULT_URL # If no url is provided, uses the default url
+        url_index = '1' # If no url index is provided, uses url index 1
 
     try:
-        menus = get_api_data(url)
-    except json.JSONDecodeError: # Raises an exception if the response from the url is not a JSON
-        print('URL entered does not return a JSON object. Try https://backend-challenge-summer-2018.herokuapp.com/challenges.json?id=1')
+        menus = get_api_data(url_index)
+    except KeyError:
+        print('URL index can only be 1 or 2.')
         sys.exit()
-
 
     parents = find_parents(menus)
     branches = get_children(menus, parents)
-    classification = validate_parents(branches)
+    classification = validate_menus(branches)
     print(classification)
-    # print(f'{json.dumps(classification, sort_keys=True, indent=4)}\n')
 
 if __name__ == '__main__':
     main()
